8f25c7ce2111a6da459600e0e145f219116f4964
Ari Perez <ariperez83@gmail.com>
2016-11-14 09:34:05 -0500

    Add ProSphere and Tista

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,13 @@
 # 2008080 DS Appeals
 # 2129396 NAVA
 # 1827228 Appeals PM
+# 2129397 Tista
+# 2180611 ProSphere
+
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    if check_team_membership(2008080) || check_team_membership(2129396) || check_team_membership(1827228)
+    if check_team_membership(2008080) || check_team_membership(2129396) || check_team_membership(1827228) || check_team_membership(2129397) || check_team_membership(2180611)
       @user = User.from_omniauth(request.env["omniauth.auth"])
       sign_in_and_redirect @user
     else
