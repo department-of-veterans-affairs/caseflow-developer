1493324b89b51fa6278527345d6b5b744ce5ae47
Sjones352 <sjones@tistatech.com>
2017-05-21 14:38:17 -0400

    added new BVA-Technology standup table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get "/sprint/standup" => "sprint#standup"
+  get "/sprint/bva_standup" => "sprint#bva_standup"
   get "/sprint/issues_report" => "sprint#issues_report"
   get "/sprint/weekly_report" => "sprint#weekly_report"
   get "/sprint/incident_report" => "sprint#incident_report"
