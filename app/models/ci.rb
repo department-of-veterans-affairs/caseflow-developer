e661d4254444c4ab25f6a9ec1336ec2322a983bf
Nick Heiner <nth23@cornell.edu>
2017-06-22 15:14:28 -0400

    Do not look at every build.

--- app/models/ci.rb
+++ app/models/ci.rb
@@ -1,20 +1,24 @@
 class CI
   def initialize
+    @most_recent_build_count = 20
     begin
       @caseflow = Travis::Repository.find('department-of-veterans-affairs/caseflow')
       
       # Cache the success rate while we are in the rescue block so all errors are caught.
       success_rate
     rescue => e
+      puts "Travis health check error", e
       @init_failed = true
     end
   end
 
-  attr_reader :init_failed
+  attr_reader :init_failed, :most_recent_build_count
 
   def success_rate
     unless @success_rate
-      master_builds = @caseflow.each_build.select {|build| build.branch_info === 'master'}
+      master_builds = @caseflow.each_build
+        .select { |build| build.branch_info === 'master'}
+        .first(@most_recent_build_count)
       @success_rate ||= master_builds.select {|build| build.passed?}.count / master_builds.size.to_f
     end
 
